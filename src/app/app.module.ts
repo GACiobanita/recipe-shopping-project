310bbb2a7d39b477269787ef21b8a14e3ebac5ba
GACiobanita <ciobanita_alex@yahoo.com>
2019-11-04 13:23:18 +0000

    Shopping & Recipe Angular Project
    Creation of an Angular website to find recipes containing recipe items which will be used to populate shopping lists for future purchases.
     Current project status:
    -classes created for Header, Recipes and Shopping List
    -planned look
    -initial dropdowns(without data access) created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component'
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
+// VVVVVV Decorator
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipeItemComponent,
+  ], //register new components
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    FormsModule,
+    HttpClientModule
+  ], //allows us to add other modules
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] //tells Angular which component needs to be aware when the app starts
 })
 export class AppModule { }
