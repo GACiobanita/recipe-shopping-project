0f8a5684fea5b931eec3afc83cdcafd73338f237
GACiobanita <ciobanita_alex@yahoo.com>
2020-02-07 16:57:40 +0000

    Observables implemented
    -removed EventEmitter implementation, replaced with Observables and Subjects
    -cleaned up RecipeService ^ and RecipeComponent ^
    -ShoppingListComponent now uses a Subscription which listens to ingredient updates, changed in ShoppingListService

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,8 @@
 import { Ingredient } from "../shared/ingredient.model"
-import { EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
 
 export class ShoppingListService {
-    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    ingredientsChanged = new Subject<Ingredient[]>();
 
     private ingredients : Ingredient[] = [
         new Ingredient('Apples', 5),
@@ -15,7 +15,7 @@ export class ShoppingListService {
 
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 
     addIngredients(ingredients: Ingredient[]) {
@@ -23,6 +23,6 @@ export class ShoppingListService {
         //    this.addIngredient(ingredient); //emits alot of events however
         //}
         this.ingredients.push(...ingredients); //spread operator '...' turns an array of elements into a list of elements, else the push method would push the entire array as a single element of the other array
-        this.ingredientsChanged.emit(this.ingredients.slice());//a single emit
+        this.ingredientsChanged.next(this.ingredients.slice());//a single emit
     }
 }
